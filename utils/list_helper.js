1e139c793c0f3246c85d213545350548dab8e4e4
Sebastian Torres <seba.torres.dev@gmail.com>
2022-04-07 21:06:00 -0300

    helper functions and unit tests, step2 (#3)

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = blogs => {
   return 1
 }
 
+const totalLikes = blogs => {
+  const sumWithInitial = blogs.reduce(
+    (previousValue, currentValue) => previousValue + currentValue.likes,
+    0
+  )
+
+  return blogs.length === 0 ? 0 : sumWithInitial
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
